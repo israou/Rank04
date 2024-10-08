7b0ed6110e54fa4e7955857694ec6d9bd7176b8b
Israa Chaabi <ichaabi@e2r7p1.1337.ma>
2024-10-08 22:21:28 +0100

    microshell update

--- microshell.c
+++ microshell.c
@@ -6,243 +6,74 @@
 /*   By: ichaabi <ichaabi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 11:39:37 by ichaabi           #+#    #+#             */
-/*   Updated: 2024/10/08 15:19:42 by ichaabi          ###   ########.fr       */
+/*   Updated: 2024/10/08 22:20:54 by ichaabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <string.h>
+#include <sys/wait.h>
 
-void err(char *str)
+void	err(char *str)
 {
 	while (*str)
 		write(2, str++, 1);
 }
 
-int cd(char **argv, int i)
+int	cd(char **av, int i)
 {
 	if (i != 2)
 		return err("error: cd: bad arguments\n"), 1;
-	if (chdir(argv[1]) == -1)
-		return err("error: cd: cannot change directory to "), err(argv[1]), err("\n"), 1;
-	return 0;
+	if (chdir(av[1]) == -1)
+		return err("error: cannot change directory to "), err(av[i]), err("\n"), 1;
+	return (0);
 }
 
-void set_pipe(int has_pipe, int *fd, int end)
+void	set_pipe(int has_pipe, int *fd, int end)
 {
 	if (has_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 		err("error: fatal\n"), exit(1);
 }
 
-int	exec(char **argv, int i, char **envp)
+int	exec(char **av, int i, char **env)
 {
-	int has_pipe, fd[2], pid, status;
-	has_pipe = argv[i] && !strcmp(argv[i], "|");
+	int has_pipe, pid, status, fd[2];
+	has_pipe = av[i] && !strcmp(av[i], "|");
 
-	if (!has_pipe && !strcmp(*argv, "cd"))
-		return cd(argv, i);
+	if (!has_pipe && !strcmp(*av, "cd"))
+		return cd(av, i);
 	if (has_pipe && pipe(fd) == -1)
 		err("error: fatal\n"), exit(1);
 	if ((pid = fork()) == -1)
 		err("error: fatal\n"), exit(1);
 	if (!pid)
 	{
-		argv[i] = 0;
+		av[i] = 0;
 		set_pipe(has_pipe, fd, 1);
-		if (!strcmp(*argv, "cd"))
-			exit(cd(argv, i));
-		execve(*argv, argv, envp);
-		err("error: cannot execute "), err(*argv), err("\n"), exit(1);
+		if (!strcmp(*av, "cd"))
+			exit(cd(av, i));
+		execve(*av, av, env);
+		err("error: cannot execute "), err(*av), err("\n"), exit(1);
 	}
 	waitpid(pid, &status, 0);
 	set_pipe(has_pipe, fd, 0);
 	return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
-int main(int argc, char **argv, char **envp)
+int main(int ac, char **av, char **env)
 {
-	(void)argc;
+	(void)ac;
 	int i = 0, status = 0;
 
-	while (argv[i])
+	while (av[i])
 	{
-		argv += i + 1;
+		av += i + 1;
 		i = 0;
-		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+		while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
 			i++;
 		if (i)
-			status = exec(argv, i, envp);
+			status = exec(av, i, env);
 	}
-	return status;
+	return (status);
 }
-
-// void	err(char *str)
-// {
-// 	while (*str)
-// 		write(2, str++, 1);
-// }
-
-// int	cd(char **av, int i)
-// {
-// 	if (i != 2)
-// 		return err("error: cd: bad arguments\n"), 1;
-// 	if (chdir(av[1]) == -1)
-// 		return err("error: cd: cannot change directory to"), err(av[1]), err("\n"), 1;
-// 	return (0);
-// }
-
-// void	set_pipe(int has_pipe, int *fd, int end)
-// {
-// 	if (has_pipe & (dup2(fd[end], end) == -1) || close(fd[1]) == -1 || close(fd[0]) == -1)
-// 		err("error: fatal\n"), exit(1);
-// }
-
-// int	exec(char **av, int i, char **env)
-// {
-// 	int	has_pipe;
-// 	int	fd[2];
-// 	int	pid;
-// 	int	status;
-
-// 	has_pipe = av[i] && !strcmp(av[i], "|");
-// 	if (!has_pipe && !strcmp(*av, "cd"))
-// 		return cd(av, i);
-// 	if (has_pipe && pipe(fd) == -1)
-// 		err("error: fatal\n"), exit(1);
-// 	if ((pid = fork()) == -1)
-// 		err("error: fatal\n"), exit(1);
-// 	if (!pid)
-// 	{
-// 		av[i] = 0;
-// 		set_pipe(has_pipe, fd, 1);
-// 		if (!strcmp(*av, "cd"))
-// 			exit(cd(av, i));
-// 		execve(*av, av, env);
-// 		err("error: cannot execute "), err(*av), err("\n"), exit(1);
-// 	}
-// 	waitpid(pid, &status, 0);
-// 	set_pipe(has_pipe, fd, 0);
-// 	return WIFEXITED(status) && WEXITSTATUS(status);
-// }
-
-void	err(char *str)
-{
-	while (*str)
-		write(2, str++, 1);
-}
-
-int	cd(char **av, int i)
-{
-	if (i != 2)
-		return err("error: cd: bad arguments\n"), 1;
-	if (chdir(av[1]) == -1)
-		return err("error: cd: cannot change directory to"), err(av[1]), err("\n"), 1;
-	return (0);
-}
-
-void	set_pipe(int has_pipe, int *fd, int end)
-{
-	if (has_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-		err("error: fatal\n"), exit(1);
-}
-
-int	exec(char **av, int i, char **env)
-{
-	int	has_pipe, fd[2], pid, status;
-	has_pipe = av[i] && !strcmp(av[i], "|");
-
-	if (!has_pipe && !strcmp(*av, "cd"))
-		return cd(av, i);
-	if (has_pipe && pipe(fd) == -1)
-		err("error: fatal\n"), exit(1);
-	if ((pid = fork()) == -1)
-		err("error: fatal\n"), exit(1);
-	if (!pid)
-	{
-		av[i] = 0;
-		set_pipe(has_pipe, fd, 1);
-		if (!strcmp(*av, "cd"));
-			exit(cd(av, i));
-		execve(*av, av, env);
-		err("error: cannot execute "), err(*av), err("\n"), exit(1);
-	}
-	waitpid(pid, &status, 0);
-	set_pipe(has_pipe, fd, 0);
-	return WIFEXITED(status) && WEXITSTATUS(status);
-}
-
-void	err(char *str)
-{
-	while (*str)
-		write(2, str++, 1);
-}
-
-int	cd(char **av, int i)
-{
-	if (i != 2)
-		return err("error: cd: bad arguments\n"), 1;
-	if (chdir(av[1]) == -1)
-		return err("error: cd: cannot change directory to"), err(av[1]), err("\n"), 1;
-	return (0);
-}
-
-void	set_pipe(int has_pipe, int *fd, int end)
-{
-	if (has_pipe && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-		err("error: fatal\n"), exit(1);
-}
-
-int	exec(char **av, int i, char **env)
-{
-	int	has_pipe, fd[2], pid, status;
-	has_pipe = av[i] && !strcmp(av[i], "|");
-
-	if (!has_pipe && !strcmp(*av, "cd"))
-		return (cd(av, i));
-	if (has_pipe && pipe(fd) == -1)
-		err("error: fatal\n"), exit(1);
-	if ((pid = fork()) == -1)
-		err("error: fatal\n"), exit(1);
-	if (!pid)
-	{
-		av[i] = 0;
-		set_pipe(has_pipe, fd, 1);
-		if (!strcmp(*av, "cd"))
-			exit(cd(av, i));
-		execve(*av, av, env);
-		err("error: cannot execute \n"), err(*av), err("\n"), exit(1);
-	}
-	waitpid(pid, &status, 0);
-	set_pipe(has_pipe, fd, 0);
-	return WIFEXITED(status) && WEXITSTATUS(status);
-}
-
-
-int	exec(char **av, int i, char **env)
-{
-	int	has_pipe, fd[2], pid, status;
-	has_pipe = av[i] && !strcmp(av[i], "|");
-
-	if (!has_pipe && !strcmp(*av, "cd"))
-		return cd(av, i);
-	if (has_pipe && pipe(fd) == -1)
-		err("error: fatal\n"), exit(1);
-	if ((pid = fork()) == -1)
-		err("error: fatal\n"), exit(1);
-	if (!pid)
-	{
-		av[i] = 0;
-		set_pipe(has_pipe, fd, 1);
-		if (!strcmp(*av, "cd"))
-			exit(cd(av, i));
-		execve(*av, av, env);
-		err("error: cannot execute "), err(*av), err("\n"), exit(1);
-	}
-	waitpid(pid, &status, 0);
-	set_pipe(has_pipe, fd, 0);
-	return WIFEXITED(status) && WEXITSTATUS(status);
-}
-
-void
